613d0d3bc13c2b3d1b05904b896243124cd8ec5a
John Francis O'Grady <johnog101@gmail.com>
2023-10-06 13:45:39 +0100

    Added SNP_data scripts

--- bin/SNP_data/PLOT_strand_flipping.R
+++ bin/SNP_data/PLOT_strand_flipping.R
@@ -2,7 +2,7 @@
 
 # Use following as reference to see what we would be expecting
 #https://www.researchgate.net/figure/Different-patterns-of-allele-frequencies-in-the-EAF-plot-These-different-patterns-have_fig2_261881356
-
+library(extrafont)
 library(tidyverse)
 library(ggplot2)
 library(tidyquant)
@@ -15,14 +15,14 @@ args = commandArgs(trailingOnly=TRUE)
 # WGS file of EU samples
 # Here specify the HF file if required
 # ouput form vcftools -freq command
-WGS_file <- read.table(args[1], sep ="\t", header = F, skip = 1)
+WGS_file <- read.table("/home/workspace/jogrady/eqtl_study/eqtl_nextflow/results/SNP_data/flip_check/European_WGS_Allele_freq.frq", sep ="\t", header = F, skip = 1)
 colnames(WGS_file) <- c("CHR", "POS", "Allele_number", "Total_alleles", "REF", "ALT")
 WGS_file <- WGS_file %>% separate(., REF, into = c("REF_allele", "REF_AF"), sep = ":")
 WGS_file <- WGS_file %>% select (1,2,5,6)
 
 
 # SNP file
-Target_file <- read.table(args[2], sep = "\t", header = F, skip = 1)
+Target_file <- read.table("/home/workspace/jogrady/eqtl_study/eqtl_nextflow/results/SNP_data/flip_check/Reference_Allele_freq.frq", sep = "\t", header = F, skip = 1)
 colnames(Target_file) <- c("CHR", "POS", "Allele_number", "Total_alleles", "REF", "ALT")
 Target_file <- Target_file %>% separate(., REF, into = c("T_REF_allele", "T_REF_AF"), sep = ":")
 Target_file <- Target_file %>% select(1,2,5,6)
@@ -39,35 +39,14 @@ merged$T_REF_AF <- as.numeric(merged$T_REF_AF)
 ## Correlation
 # note 3 rows have missing values which are not included
 # These are Insertions not picked up by genotyping platform
-
+dev.off()
 merged = drop_na(merged)
 test = cor.test(merged$REF_AF, merged$T_REF_AF, method = "spearman", exact = F)
-test$estimate
-write.table(merged, "RAW_AF_Comparsion_Values.txt", row.names = F, col.names = T)
+test
+#args[3]
+write.table(merged, args[3], row.names = F, col.names = T)
 # Ggplot - 
-ggplot(data = merged, aes(x = REF_AF, y = T_REF_AF)) +
-  geom_point(size = 0.25, color = "#2c7bb6", alpha = 1) +
-  labs(x = "Allele frequency in European WGS reference set", 
-       y = "Allele frequency in target set") +
-  scale_y_continuous(breaks = seq(0, 1, 0.2)) +
-  scale_x_continuous(breaks = seq(0, 1, 0.2)) +
-  annotate('segment', x = 0, xend = 1, y = 0, yend = 1,
-           linewidth = 1,
-           alpha = 0.8,
-           color = "#d7191c") +
-  theme_tq(base_size = 14) +
-  theme(axis.title  = element_text(colour = "black"))
 
-args[3]
-ggsave(args[3], width=10, height=8, dpi = 600)
-
-png(args[4], width = 10, height = 8, units = "in", res = 600)
-smoothScatter(merged$REF_AF,merged$T_REF_AF,
-              pch = 10, col = "red", nrpoints = 150, bandwidth = 0.04, ret.selection = F,
-              nbin = 120, xlab = "Allele frequency in European WGS reference set",
-              ylab = "Allele frequency in target set")
-
-dev.off()
 
 
 # base R
@@ -81,6 +60,39 @@ Points.2.remove
 SNPs.2.remove <- merged[Points.2.remove,] %>% select(CHR, POS, REF_AF,T_REF_AF)
 SNPs.2.remove.positions <- merged[Points.2.remove,] %>% select(CHR, POS)
 
-colnames(SNPs.2.remove) <- c("CHR", "POS", "WGS_REF_AF", "Target_REF_AF")
+
+
+# Highlight this on the original graph
+merged$rows <- 1:nrow(merged)
+SNPs.2.remove$rows <- rownames(SNPs.2.remove)
+
+merged$spurious <- if_else(merged$rows %in% SNPs.2.remove$rows, "#2166ac", "#b2182b")
+head(merged)
+ggplot(data = merged, aes(x = REF_AF, y = T_REF_AF, colour = spurious)) +
+  geom_point(size = 1, alpha = 0.8) +
+  labs(x = "Allele frequency in European WGS reference set", 
+       y = "Allele frequency in target set") +
+  scale_y_continuous(breaks = seq(0, 1, 0.2)) +
+  scale_x_continuous(breaks = seq(0, 1, 0.2)) +
+  annotate('segment', x = 0, xend = 1, y = 0, yend = 1,
+           linewidth = 1,
+           alpha = 1,
+           color = "orange") +
+  theme_bw(base_size = 14, base_family="calibri") +
+  theme(axis.title  = element_text(colour = "black")) +
+  theme(axis.text.x = element_text(angle = 0, size = 15, colour = "black"),
+        axis.text.y = element_text(angle = 0, size = 15, colour = "black"),
+        axis.title.y = element_text(size = 21, color = "black"),
+        axis.title.x = element_text(size = 21, color = "black"),
+         legend.position = "none") +
+         geom_smooth(method = "lm", col = "black")
+ggsave("Allele_frequencies.png", width=10, height=8, dpi = 300)
+dim(merged)
+
+
+
+
+colnames(SNPs.2.remove) <- c("CHR", "POS", "WGS_REF_AF", "Target_REF_AF", "rows")
+SNPs.2.remove <- SNPs.2.remove[,1:4]
 write.table(SNPs.2.remove, file = args[5], quote = F, col.names = T, row.names = F, sep = "\t")
 write.table(SNPs.2.remove.positions, file = args[6], quote = F, col.names = F, row.names = F, sep = "\t")
